d1be6a089865026cd84609cd1361a5913f06e938
Joshuaultra <70752622+Joshuaultra@users.noreply.github.com>
2020-09-04 15:43:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '*';
+
+client.once('ready', () => {
+    console.log('MAVELI BOT is online');
+      client.user.setActivity('*help')
+
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'anugrahichalum'){
+        message.channel.send('Noominte anugraham epolum koodeyundavum');
+    } else if (command == 'gf'){
+        message.channel.send('Antreasa');
+    }else if (command == 'youtube'){
+        message.channel.send('https://www.youtube.com/channel/UCvY-ehTz17Wrav4Rr5wd24A');
+    }else if (command == 'specs'){
+        message.channel.send('i9 9900K,RTX 3070,32GB Ram');
+    }else if (command== 'help'){
+        message.channel.send('Commands are *anugrahichalum *gf *youtube *specs');
+    }
+});
+
+client.login('NzUwMjQ4NDkyODYwOTY0OTI0.X03xWw.163MoZ_qa7ksgqCaA_47VuahKhg');
