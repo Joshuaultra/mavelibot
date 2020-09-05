5d3d71c3743e84ab5feec916ee7305d0972affe8
Joshuaultra <70752622+Joshuaultra@users.noreply.github.com>
2020-09-05 11:46:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -18,14 +18,21 @@ client.on('message', message =>{
 
     if(command === 'anugrahichalum'){
         message.channel.send('Noominte anugraham epolum koodeyundavum');
-    } else if (command == 'gf'){
-        message.channel.send('Antreasa');
+    } else if (command == 'website'){
+        message.channel.send('https://maveli.herokuapp.com/');
     }else if (command == 'youtube'){
         message.channel.send('https://www.youtube.com/channel/UCvY-ehTz17Wrav4Rr5wd24A');
     }else if (command == 'specs'){
         message.channel.send('i9 9900K,RTX 3070,32GB Ram');
+    }else if (command == 'gf'){
+        message.channel.send('Antreasa');
     }else if (command== 'help'){
-        message.channel.send('Commands are *anugrahichalum *gf *youtube *specs');
+        message.channel.send('Commands Are
+                             *anugrahichalum
+                             *website
+                             *youtube
+                             *specs
+                             *gf');
     }
 });
 
